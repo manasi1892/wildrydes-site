b56ffbcc30151130f748254676730ef04206c0db
manasi1892 <manasi.shah92@gmail.com>
2025-04-10 11:13:53 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_SqKrhcc6u', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4sp112hj3bunjtin90ofjr4dgd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
